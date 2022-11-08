e8d43a5bed60906a1ff7e0d6e0a5ab33f90493a6
Vincent Fuertez <vfuertez@gmail.com>
2022-11-08 00:29:25 -0500

    Done

--- server.js
+++ server.js
@@ -3,10 +3,6 @@ const express = require('express');
 const app = express();
 const PORT = 3003;
 
-app.get('/', (request, response) => {
-    response.send(`<h2>99 Bottles of beer on the wall</h2>`)
-})
-
 
 app.get('/magic/:question', (request, response) => {
     const arr = ["It is certain", "It is decidedly so", "Without a doubt", "Yes definitely","You may rely on it", "As I see it yes", "Most likely", "Outlook good","Yes", "Signs point to yes", "Reply hazy try again", "Ask again later","Better not tell you now", "Cannot predict now", "Concentrate and ask again","Don't count on it", "My reply is no", "My sources say no","Outlook not so good", "Very doubtful"];
